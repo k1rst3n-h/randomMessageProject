3ed3ee169832b4e87122ae60d94d20c0e13ebfa9
kirst <kirstenhodge3@gmail.com>
2025-04-18 16:48:49 +0200

    function for the rndmMessage button

--- randommsg.js
+++ randommsg.js
@@ -1,4 +1,16 @@
-const button = document.querySelector("btnforjs");
-const output = document.querySelector("OutputText");
+const button = document.querySelector("#btnforjs");
+const output = document.querySelector("#outputText");
+
+const message = [
+  "Hey there!",
+  "Drink some water.",
+  "Take a breath.",
+  "Eat some food."
+]
+button.addEventListener("click", function() {
+  const randomIndex = Math.floor(Math.random() * message.length);
+  const randomMessage = message[randomIndex];
+  output.textContent = randomMessage;
+})
 
 
